28e0693a4d00cf7675b852332cb3152bd76f5fea
TangRufus <tangrufus@gmail.com>
2020-12-31 01:16:22 +0000

    composer require --dev lucatume/wp-browser codeception/module-asserts codeception/module-phpbrowser codeception/module-webdriver codeception/module-db codeception/module-filesystem codeception/module-cli codeception/util-universalframework && vendor/bin/codecept init --namespace=Dummy\Dummy wpbrowser

---
+++ tests/_support/Helper/Functional.php
@@ -0,0 +1,10 @@
+<?php
+namespace DummyDummy\Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Functional extends \Codeception\Module
+{
+
+}
